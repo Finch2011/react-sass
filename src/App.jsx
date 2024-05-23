b9df3c7ef9f747531faea42f02873d5653cfcc49
yocom <you@example.com>
2024-05-23 17:47:47 +0330

    add 3 hook for project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import Product from "./components/Product"
+import React , {useState , useRef , useEffect}  from  "react"; 
 
-// import { Link } from 'react-router-dom'
 function App() {
- 
-
+  const [number , setNumber] = useState(false)
+  const showNumber = useRef();
+  // function UseState(){
+  //   setNumber( number => number - 1 )
+  //   if(number<= 0){
+  //     showNumber.current.style.color = 'red'
+  //   }
+  // }
+  // function UseState2(){
+  //   setNumber( number => number + 1)
+  //   if(number>= 0){
+  //     showNumber.current.style.color = 'green'
+  //   }
+    
+  // }
+  
   return (
     
     <div className="main-App">
@@ -11,7 +24,15 @@ function App() {
      <h1 className="welcom-text">Welcome To <span className="sass-text purple ">Sass</span> 101</h1>    
      <p className="larnMore-text">The best pre-proccesor for css</p>
      <br />
-     <button className="button-App purple">Larn More</button>
+    <p className="craeteNumber">  <span ref={showNumber}>{number ? "PARS ElX pric : 5,000 $" : "PARS XUM pric : 10,000 $"}</span> </p>
+    <br />
+     <button onClick={()=>{
+      setNumber(!number)
+     }} className="button-App purple">{number ? "Low Option" : "Full Option"}</button>
+    <br />
+     {/* <button onClick={UseState} className="button-App purple">Low-off </button> */}
+     <br />
+     <p></p>
     </div>
     <div className="User purple">
      <img className="imges" src="/icons8-user-90.png" alt="" />
